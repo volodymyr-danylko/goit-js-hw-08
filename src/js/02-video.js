022b949e31a56d034a41c568ec34367cb818a1e9
Volodymyr Danylko <danylko91@gmail.com>
2022-10-20 20:50:31 +0300

    fineshed second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const LOCAL_STORAGE_VIMEO_PLAYER_KEY = 'videoplayer-current-time';
+const storageData = localStorage.getItem(LOCAL_STORAGE_VIMEO_PLAYER_KEY);
+
+const player = new Player('vimeo-player');
+
+if (storageData) {
+  player.setCurrentTime(storageData);
+}
+
+function onPlay(data) {
+  localStorage.setItem(LOCAL_STORAGE_VIMEO_PLAYER_KEY, JSON.stringify(data.seconds));
+}
+
+player.on('timeupdate', throttle(onPlay, 1000));
